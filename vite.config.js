abb672ba68a50384bbb5c5dde9e0fb16291029ff
aliatwood <aliatwood0104@gmail.com>
2026-02-05 18:10:20 +0100

    uppdatera karaktar.scss
    Jag la till lite style i karaktar.scss filen

--- vite.config.js
+++ vite.config.js
@@ -10,8 +10,6 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, "src/index.html"),
-        bilder: resolve(__dirname, "src/bilder.html"),
-        process: resolve(__dirname, "src/process.html")
       }
     }
   }
