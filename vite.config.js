32e982a83c01621c8312418ea837e7e6414e4cc8
aliatwood <aliatwood0104@gmail.com>
2026-02-09 20:01:34 +0100

    Uppdatera bilderna
    La till bildoptimering och lazy loading

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,19 @@
 import { defineConfig } from "vite";
 import { resolve } from "path";
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
 export default defineConfig({
   base: '/labb3-CSS-Preproccesorer/',
+  plugins: [
+    ViteImageOptimizer({
+      jpg: {
+        quality: 80,
+      },
+      png: {
+        quality: 80,
+      },
+    }),
+  ],
   build: {
     outDir: "docs",
     emptyOutDir: true,
